c9db781e0ea3e6d370debbe8c10d7add360fea43
Christian Bläul <christian.blaeul@startnext.com>
2020-11-29 23:44:38 +0100

    Allow setting angle using the numpad

--- src/objects/Ship.ts
+++ src/objects/Ship.ts
@@ -9,6 +9,9 @@ class Ship extends ObjectAffectedByGravity {
     private stopAngularVel: boolean = false;
     private stopLinearVel: boolean = false;
     private text: Phaser.GameObjects.Text;
+    private targetAngle: number | false = false;
+    private directionToTargetAngle: number = 0;
+    private angleBreakingNeeded: Boolean = false;
 
     constructor(scene: Phaser.Scene, image: Phaser.Types.Physics.Arcade.ImageWithDynamicBody) {
         super(scene, image);
@@ -22,6 +25,31 @@ class Ship extends ObjectAffectedByGravity {
         scene.input.on('pointerdown', (pointer: PointerEvent) => {
             scene.add.image(pointer.x, pointer.y - 60, this.fire.texture);
         }, this);
+
+        scene.input.keyboard.on('keydown-NUMPAD_ONE', () => {
+            this.targetAngle = 3 / 4 * Math.PI
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_TWO', () => {
+            this.targetAngle = 2 / 4 * Math.PI
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_THREE', () => {
+            this.targetAngle = 1 / 4 * Math.PI
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_FOUR', () => {
+            this.targetAngle = Math.PI
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_SIX', () => {
+            this.targetAngle = 0
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_SEVEN', () => {
+            this.targetAngle = 5 / 4 * Math.PI
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_EIGHT', () => {
+            this.targetAngle = 6 / 4 * Math.PI
+        });
+        scene.input.keyboard.on('keydown-NUMPAD_NINE', () => {
+            this.targetAngle = 7 / 4 * Math.PI
+        });
     }
 
     private setRandomColor() {
@@ -62,12 +90,16 @@ class Ship extends ObjectAffectedByGravity {
                     this.body.setVelocity(0);
                 } else {
                     this.body.acceleration.setToPolar(this.body.velocity.angle() - Math.PI, 2 * speed);
-                    new FireParticleEmitter(this.fire, this.img, this.scene.game, -2);
+                    this.fireThruster(-2);
                 }
             } else {
                 this.img.setAcceleration(0);
             }
         }
+    }
+
+    private fireThruster(angularAcc: number = 0) {
+        new FireParticleEmitter(this.fire, this.img, this.scene.game, angularAcc);
     }
 
     private handleRotation(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
@@ -89,9 +121,59 @@ class Ship extends ObjectAffectedByGravity {
             angularAcc = 1;
         }
         if (angularAcc) {
-            new FireParticleEmitter(this.fire, this.img, this.scene.game, Math.sign(angularAcc));
+            this.fireThruster(Math.sign(angularAcc));
+            this.targetAngle = false;
+            this.directionToTargetAngle = 0;
+        }
+        if (this.targetAngle !== false) {
+            let angleDifference = this.getAngleDifference();
+            const absAngleDiff = Math.abs(angleDifference);
+            if (absAngleDiff > Math.PI){
+                angleDifference = -1 * angleDifference;
+            }
+            console.log(angleDifference, this.targetAngle);
+            if (absAngleDiff < 0.5 && this.angleBreakingNeeded) {
+                if (this.directionToTargetAngle)
+                    this.fireThruster(-this.directionToTargetAngle);
+                this.img.setAngularAcceleration(this.body.angularAcceleration / 2);
+            }
+            if (absAngleDiff < 0.1) {
+                this.angleBreakingNeeded = false;
+                this.img.setRotation(this.targetAngle);
+                this.img.setAngularVelocity(0);
+                this.targetAngle = false;
+                if (this.directionToTargetAngle)
+                    this.fireThruster(-this.directionToTargetAngle);
+                this.directionToTargetAngle = 0;
+            } else if (!this.directionToTargetAngle) {
+                this.directionToTargetAngle = -1 * Math.sign(angleDifference);
+                this.fireThruster(this.directionToTargetAngle);
+                angularAcc = 10 * this.directionToTargetAngle;
+                this.angleBreakingNeeded = true;
+                this.scene.time.delayedCall(100, this.accelerateRotation, [this])
+                this.scene.time.delayedCall(150, this.accelerateRotation, [this])
+                this.scene.time.delayedCall(200, this.accelerateRotation, [this])
+            }
         }
         this.img.setAngularAcceleration(400 * angularAcc);
+    }
+
+    private getAngleDifference(): number {
+        if (this.targetAngle === false) return 0;
+        let angleDifference = this.img.rotation - this.targetAngle;
+        if (angleDifference > 2 * Math.PI) angleDifference -= 2 * Math.PI;
+        if (angleDifference < -2 * Math.PI) angleDifference += 2 * Math.PI;
+        return angleDifference;
+    }
+
+    private accelerateRotation(ship: Ship | null = null) {
+        if (ship === null) ship = this;
+        if (Math.abs(ship.getAngleDifference()) < 0.4) {
+            // No acceleration needed
+            return;
+        }
+        ship.fireThruster(ship.directionToTargetAngle);
+        ship.img.setAngularAcceleration(200 * ship.directionToTargetAngle);
     }
 }
 
